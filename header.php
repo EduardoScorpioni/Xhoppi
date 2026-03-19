8dbfa51bd83f05f6b68877dfd96baee6046e6705
Eduardo <eduscorpioni@outlook.com>
2026-03-18 21:52:55 -0300

    primeira parte do header e footer em desenvolvimento

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="header.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <section class = "cabeçalho">
+            <section class="ladinho" >
+            <a href="index.html"> <img src="img/logo.png" alt=""></a>
+            <a href=""><h2>Xhoppi</h2></a>
+            <a href="login.html"> Entre</a>
+            </section>
+            <section class="outro_ladinho">
+            <a href="login.html"> Precisa de ajuda?</a>
+            </section>
+        </section>
+    </header>
+</body>
+</html>
