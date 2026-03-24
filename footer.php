ebc4a294ad0405f56a8a108edd2caa77c6cf70da
Eduardo <eduscorpioni@outlook.com>
2026-03-23 22:47:29 -0300

    Correção dos css e links, organização de codigo, e inicio do footer com css usando gridLayout

--- footer.php
+++ footer.php
@@ -1,13 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="CSS/footer.css">
+<title>Document</title>
 </head>
 <body>
-    <footer>
-        <section> teste3222</section>
-    </footer>
+<footer class="footer">
+
+        <section class="coluna">
+        <h3>ATENDIMENTO AO CLIENTE</h3>
+        <a href="#">CentFral de Ajuda</a>
+        <a href="#">Como Comprar</a>
+        <a href="#">Métodos de Pagamento</a>
+        <a href="#">Garantia</a>
+        </section>
+
+        <section class="coluna">
+        <h3>SOBRE</h3>
+        <a href="#">Sobre Nós</a>
+        <a href="#">Políticas</a>
+        <a href="#">Privacidade</a>
+        </section>
+
+        <section class="coluna">
+        <h3>PAGAMENTO</h3>
+        <img src="" alt="">
+        <img src="" alt="">
+        <img src="" alt="">
+        <img src="" alt="">
+        <img src="" alt="">
+        <img src="" alt="">
+        <img src="" alt="">
+        </section>
+
+        
+        <section class="coluna">
+        <h3>SIGA-NOS</h3>
+        </section>
+
+        <section class="coluna">
+        <h3>APP</h3>
+        </section>
+        <section class = "copy">
+        <p>&copy; 2026 Xhoppi. Todos os direitos acadêmicos reservados.</p>
+
+        </section>
+</footer>
 </body>
 </html>
