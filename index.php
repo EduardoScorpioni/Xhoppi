eb98ffd55030f93315a28ff182f5ae3a830d4018
Eduardo <eduscorpioni@outlook.com>
2026-03-24 00:17:21 -0300

    footer implementado no index, e criação dos cards de produtos ja finalizada

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="CSS/index.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
         <title>Document</title>
     </head>
@@ -43,5 +44,122 @@
             <img src="img/banner.png" alt="">
           </div>
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-    </body>
+          <h2 class="titulo-secao">DESCOBEERTAS DO DIA</h2>
+          <hr class ="linha-capenga">
+          <section class = "produtos">
+                <section class="card">
+                  <img src="img/produto1.png" alt="">
+                  <p class="titulo">Camisa de Desenvolvedor Front-end</p>
+                    <section class = "info">
+                      <span class ="preco"> R$998,98</span>
+                      <span class ="estoque"> 171 disponiveis </span>
+                    </section>
+                </section>
+
+                <section class="card">
+                  <img src="img/produto1.png" alt="">
+                  <p class="titulo">Camisa de Desenvolvedor Front-end</p>
+                    <section class = "info">
+                      <span class ="preco"> R$998,98</span>
+                      <span class ="estoque"> 171 disponiveis </span>
+                    </section>
+                </section>
+
+                <section class="card">
+                  <img src="img/produto1.png" alt="">
+                  <p class="titulo">Camisa de Desenvolvedor Front-end</p>
+                    <section class = "info">
+                      <span class ="preco"> R$998,98</span>
+                      <span class ="estoque"> 171 disponiveis </span>
+                    </section>
+                </section>
+
+                <section class="card">
+                  <img src="img/produto1.png" alt="">
+                  <p class="titulo">Camisa de Desenvolvedor Front-end</p>
+                    <section class = "info">
+                      <span class ="preco"> R$998,98</span>
+                      <span class ="estoque"> 171 disponiveis </span>
+                    </section>
+                </section>
+                
+                <section class="card">
+                  <img src="img/produto1.png" alt="">
+                  <p class="titulo">Camisa de Desenvolvedor Front-end</p>
+                    <section class = "info">
+                      <span class ="preco"> R$998,98</span>
+                      <span class ="estoque"> 171 disponiveis </span>
+                    </section>
+                </section>
+
+                <section class="card">
+                  <img src="img/produto1.png" alt="">
+                  <p class="titulo">Camisa de Desenvolvedor Front-end</p>
+                    <section class = "info">
+                      <span class ="preco"> R$998,98</span>
+                      <span class ="estoque"> 171 disponiveis </span>
+                    </section>
+                </section>
+
+                <section class="card">
+                  <img src="img/produto1.png" alt="">
+                  <p class="titulo">Camisa de Desenvolvedor Front-end</p>
+                    <section class = "info">
+                      <span class ="preco"> R$998,98</span>
+                      <span class ="estoque"> 171 disponiveis </span>
+                    </section>
+                </section>
+
+                <section class="card">
+                  <img src="img/produto1.png" alt="">
+                  <p class="titulo">Camisa de Desenvolvedor Front-end</p>
+                    <section class = "info">
+                      <span class ="preco"> R$998,98</span>
+                      <span class ="estoque"> 171 disponiveis </span>
+                    </section>
+                </section>
+
+                <section class="card">
+                  <img src="img/produto1.png" alt="">
+                  <p class="titulo">Camisa de Desenvolvedor Front-end</p>
+                    <section class = "info">
+                      <span class ="preco"> R$998,98</span>
+                      <span class ="estoque"> 171 disponiveis </span>
+                    </section>
+                </section>
+
+                <section class="card">
+                  <img src="img/produto1.png" alt="">
+                  <p class="titulo">Camisa de Desenvolvedor Front-end</p>
+                    <section class = "info">
+                      <span class ="preco"> R$998,98</span>
+                      <span class ="estoque"> 171 disponiveis </span>
+                    </section>
+                </section>
+                
+                <section class="card">
+                  <img src="img/produto1.png" alt="">
+                  <p class="titulo">Camisa de Desenvolvedor Front-end</p>
+                    <section class = "info">
+                      <span class ="preco"> R$998,98</span>
+                      <span class ="estoque"> 171 disponiveis </span>
+                    </section>
+                </section>
+                
+                <section class="card">
+                  <img src="img/produto1.png" alt="">
+                  <p class="titulo">Camisa de Desenvolvedor Front-end</p>
+                    <section class = "info">
+                      <span class ="preco"> R$998,98</span>
+                      <span class ="estoque"> 171 disponiveis </span>
+                    </section>
+                </section>
+
+                
+         
+          </section>
+        
+        
+          <?php include_once "footer.php" ?> 
+      </body>
 </html>
