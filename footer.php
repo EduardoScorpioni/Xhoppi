3cd39adc2a0b96f928476842c233a33990397a1f
Eduardo <eduscorpioni@outlook.com>
2026-03-23 23:31:12 -0300

    Footer finalizado, adição de imagens

--- footer.php
+++ footer.php
@@ -3,6 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 <link rel="stylesheet" href="CSS/footer.css">
 <title>Document</title>
 </head>
@@ -11,42 +12,62 @@
 
         <section class="coluna">
         <h3>ATENDIMENTO AO CLIENTE</h3>
-        <a href="#">CentFral de Ajuda</a>
+        <a href="#">Central de Ajuda</a>
         <a href="#">Como Comprar</a>
         <a href="#">Métodos de Pagamento</a>
-        <a href="#">Garantia</a>
+        <a href="#">Garantia Xhoppi</a>
+        <a href="">Devolução e Reembolso</a>
+        <a href="">Fale Conosco</a>
+        <a href="">Ouvidoria</a>
         </section>
 
         <section class="coluna">
-        <h3>SOBRE</h3>
+        <h3>SOBRE A Xhoppi</h3>
         <a href="#">Sobre Nós</a>
-        <a href="#">Políticas</a>
-        <a href="#">Privacidade</a>
+        <a href="#">Politicas Xhoppi</a>
+        <a href="#">Programa de Ailiados da Xhoppi</a>
+        <a href="">Seja um Entregador Xhopii</a>
+        <a href="">Ofertas Relámpago</a>
+        <a href="">Xhopii Blog</a>
+        <a href="">Empresa</a>
         </section>
-
+        
         <section class="coluna">
         <h3>PAGAMENTO</h3>
-        <img src="" alt="">
-        <img src="" alt="">
-        <img src="" alt="">
-        <img src="" alt="">
-        <img src="" alt="">
-        <img src="" alt="">
-        <img src="" alt="">
+            <section class = "imagens">
+                <img src="img/Pagamento/pix.png" alt="">
+                <img src="img/Pagamento/boleto.png" alt="">
+                <img src="img/Pagamento/elo.png" alt="">
+                <img src="img/Pagamento/master.png" alt="">
+                <img src="img/Pagamento/hiper.png" alt="">
+                <img src="img/Pagamento/visa.png" alt="">
+            </section>
         </section>
 
         
         <section class="coluna">
         <h3>SIGA-NOS</h3>
+        <i class="bi bi-instagram"> Instagram</i>
+        <i class="bi bi-twitter"> Twitter</i>
+        <i class="bi bi-facebook"> Facebook</i>
+        <i class="bi bi-youtube"> Youtube</i>
+        <i class="bi bi-linkedin"> Linkedin</i>
+
         </section>
 
         <section class="coluna">
-        <h3>APP</h3>
+        <h3>Atendimento ao Cliente</h3>
+        <img src="img/App/Qr.png" alt="">
+        <img src="img/App/play.png" alt="">
+        <img src="img/App/apple.png" alt="">
         </section>
+        
         <section class = "copy">
-        <p>&copy; 2026 Xhoppi. Todos os direitos acadêmicos reservados.</p>
+            <hr>
+            <p align ="center">&copy; 2026 Xhoppi. Todos os direitos acadêmicos reservados.</p>
 
         </section>
 </footer>
-</body>
+</footer>
+    </body>
 </html>
