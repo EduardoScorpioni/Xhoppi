3422a1774b76d4ebe3c96d80e41eef07fb121a39
Igor Marques da Silva <ighormarques21@gmail.com>
2026-03-29 23:31:35 -0300

    Add Tela 'redefinir'

---
+++ redefinir.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Xhopii - Login</title>
+        <link rel="stylesheet" href="CSS/redefinir.css">
+    </head>
+    <body>
+        <header class="topbar">
+            <div class="top-left">
+                <img src="img/logo.png" class="logo">
+                <span class="brand">Xhopii</span>
+                <span class="enter">Entre</span>
+            </div>
+            <div class="top-right">
+                <a href="#">Precisa de ajuda?</a>
+            </div>
+        </header>
+        <div class="conteiner">
+            <div class="card-redefinir">
+                <a href="login.html" class="voltar">&#8592;</a>
+                <h2>Redefinir Senha</h2>
+                <input type="email" placeholder="Email">
+                <button class="btn-enviar">Enviar</button>
+            </div>
+        </div>
+        <?php include "footer.php"?>
+    </body>
+</html>
